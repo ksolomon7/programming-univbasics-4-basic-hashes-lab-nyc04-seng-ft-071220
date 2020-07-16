43f60d12cb9b00ab689434e60f87c4d641157e8b
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-16 17:43:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,12 @@
 def new_hash
-  # return an empty hash
+  new_hash.new# return an empty hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash={
+    name: "Kalkidan",
+    age: 100
+  }# return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
